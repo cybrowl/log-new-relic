a75f7af3b352a64bdc4c332b3f96227448ff53c7
cyberowl <cybrowl@protonmail.com>
2023-03-19 22:45:17 -0700

    add: script to send data

---
+++ index.js
@@ -0,0 +1,63 @@
+import fetch from "node-fetch";
+import { config } from "dotenv";
+
+config();
+
+const newRelicApiKey = process.env.NEW_RELIC_API_KEY;
+
+const NEW_RELIC_LOG_API_URL = "https://log-api.newrelic.com/log/v1";
+const HTTP_ENDPOINT_URL =
+  "https://ree2h-zaaaa-aaaag-aba5q-cai.raw.ic0.app/metrics";
+
+const headers = {
+  "Content-Type": "application/json",
+  "X-Insert-Key": newRelicApiKey,
+};
+
+async function fetchData() {
+  try {
+    const response = await fetch(HTTP_ENDPOINT_URL);
+
+    console.log("response: ", response);
+
+    if (response.ok) {
+      return await response.json();
+    } else {
+      console.error(`Error fetching data: ${response.status}`);
+      return null;
+    }
+  } catch (error) {
+    console.error(`Error fetching data: ${error.message}`);
+    return null;
+  }
+}
+
+async function forwardToNewRelic(data) {
+  if (data) {
+    try {
+      const response = await fetch(NEW_RELIC_LOG_API_URL, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(data),
+      });
+
+      if (response.status === 202) {
+        console.log("Data sent to New Relic successfully");
+      } else {
+        console.error(`Error sending data to New Relic: ${response.status}`);
+      }
+    } catch (error) {
+      console.error(`Error sending data to New Relic: ${error.message}`);
+    }
+  }
+}
+
+(async function main() {
+  while (true) {
+    const data = await fetchData();
+
+    console.log("data: ", data);
+    await forwardToNewRelic(data);
+    await new Promise((resolve) => setTimeout(resolve, 60 * 1000)); // Adjust the sleep interval as needed
+  }
+})();
